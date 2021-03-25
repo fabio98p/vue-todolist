ce40afc10b3ffbea3520b32279adbd283580ad41
fabio98p <parisifabio98@gmail.com>
2021-03-25 15:03:11 +0100

    todo fine html e css

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+	console.log('ciao ale');
+
+	var root = new Vue({
+		el: '#root',
+		data: {
+
+
+			
+		},
+		methods: {
+
+
+		},
+		computed: {
+			// // a computed getter
+			// reversedMessage: function () {
+			// 	// `this` points to the vm instance
+			// 	return this.message.split('').reverse().join('')
+			// }
+		},
+		created: function () {
+
+		},
+	})
+});
